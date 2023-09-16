dc45136c1cd5a5026ad1574f7096e7fdf8aea632
Filipe Santos <filipssant@gmail.com>
2023-09-15 21:46:53 -0300

    amapacap footer

--- src/components/HomeFooter/HomeFooter.tsx
+++ src/components/HomeFooter/HomeFooter.tsx
@@ -12,13 +12,21 @@ const HomeFooter = () => {
     <Container>
       <BottomSection>
         <CompanyColumn>
-          <CompanyName>Maracap - TÍTULO DE CAPITALIZAÇÃO</CompanyName>
-          <CompanyLogo src="/brcap.png" />
+          <CompanyName>Amapá CAP - TÍTULO DE CAPITALIZAÇÃO</CompanyName>
+          <CompanyLogo src="/cactvs.png" />
         </CompanyColumn>
         <TermsColumn>
-          Escritório Central: Av. São Luís Rei de França, 12 – Olho D’Água CEP
-          65067-485 São Luis / MA Central de Atendimento:
-          atendimento@maracap.com.br – (98) 3303-3000
+          O BILHETE PREMIÁVEL AMAPA CAP, é uma promoção comercial promovida
+          pelas empresas do Grupo CACTVS CACTVS INSTITUICAO DE PAGAMENTO
+          S.A.CNPJ 39.696.395/0001-44, CACTVS CORRETORA DE SEGUROS CNPJ
+          39.376.932/0001-79 e CACTVS MARKETPLACE CNPJ 39.942.751/0001-62,
+          aprovada pela SEAE/MF sob código/número Promoção Comercial 2022/06578
+          –AP e PROCESSO N.° 17377.004153/2022-74 – AP, aprovada pela SEAE/MF
+          Certificado de Autorização nº 01.021911/2022 - AP, referente ao
+          Processo nº 17377.004153/2022-74 – AP, com base legal na Lei Federal
+          nº 5.768/1971. Poderão ser consultadas no endereço eletrônico clicando
+          aqui, de acordo com o número da SEAE processo constante neste
+          regulamento. Prêmios líquidos de imposto de renda.
         </TermsColumn>
       </BottomSection>
     </Container>
