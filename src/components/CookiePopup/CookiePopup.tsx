291c26fb3bc55e607ac7936e3af768ffce867123
Filipe Santos <filipssant@gmail.com>
2023-09-21 10:11:58 -0300

    add pop up component

---
+++ src/components/CookiePopup/CookiePopup.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import {Container} from './styles';
+
+interface CookiePopupProps {
+  onAccept: () => void;
+}
+
+const CookiePopup: React.FC<CookiePopupProps> = ({ onAccept }) => {
+  const [showPopup, setShowPopup] = useState(true);
+
+  const handleAccept = () => {
+    setShowPopup(false);
+    onAccept();
+  };
+
+  return (
+    showPopup && (
+      <Container>
+        <div className="cookie-content">
+          <h2>Política de Privacidade</h2>
+          <p>Texto da sua política de privacidade aqui...</p>
+          <button onClick={handleAccept}>Aceitar Cookies</button>
+        </div>
+      </Container>
+    )
+  );
+};
+
+export default CookiePopup;
