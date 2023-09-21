2b986893e18fd46939fc20b69cc58f138e3ade42
Filipe Santos <filipssant@gmail.com>
2023-09-21 10:12:42 -0300

    add popup styled components

---
+++ src/components/CookiePopup/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+  position: absolute;
+  z-index: 9999;
+  inset: 0;
+  
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
